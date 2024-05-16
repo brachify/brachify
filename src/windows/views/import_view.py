2fc51c74fe6624f751df0d541345b0f1e168c7a4
SamuelStreet <samuelcstreet@gmail.com>
2024-05-16 12:37:44 -0700

    fix 1) - can go back to import, not yet fully fixed

--- src/windows/views/import_view.py
+++ src/windows/views/import_view.py
@@ -37,7 +37,7 @@ class ImportView(CustomView):
 
         window.dicommodel.update(data)
         window.displaymodel.set_transparent(True)
-        get_app().window.ui.viewswidget.setCurrentIndex(4)
+        app.signals.viewChanged.emit(4)
 
     def action_update_import_label(self, data:DicomData):
         self.ui.label_file_info.setText(data.toString())
