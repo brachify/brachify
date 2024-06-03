36131796ebe70aac589babd0787a802b86fdd581
stephaniemerkl <s.merkl@telus.net>
2024-06-03 14:23:30 -0700

    Create resetAllValues() method in load.py.
    This method takes a dictionary of spin box values (such as a config dictionary) and resets all the spin box values and applies those settings.

--- src/settings/load.py
+++ src/settings/load.py
@@ -5,6 +5,7 @@ For loading default settings.
 from classes.info import DIR_PATH, HOME_PATH, USER_PATH, RESOURCES_PATH
 import json
 from classes.logger import log
+from classes.app import get_app
 
 config_values = None
 
@@ -39,5 +40,63 @@ def load_config_file():
     return config_values
 
 
+def resetAllValues(values_dict: dict):
+    """
+    Resets all values in spin boxes to the values in the given dictionary and applies them to the views.
+    """
+    ###############################
+    # Will need to change this function later after config-load-pop-up PR is merged to main.
+    # Move this function to be a function of the Values class, so will have to accept self as an arg.
+    ###############################
+    
+    app = get_app()
+
+    # set all the spin box values and apply the changes
+    """
+    Steps:
+    1. read the value from the dictionary
+    2. set the spin box value
+    3. apply the value
+    """
+
+    # Cylinder View values
+    cylinder_diameter = values_dict.get("CONFIG_CYLINDER_DIAMETER")
+    cylinder_length = values_dict.get("CONFIG_CYLINDER_LENGTH")
+    app.window.navigationmodel.views[1].ui.spinbox_diameter.setValue(cylinder_diameter)
+    app.window.navigationmodel.views[1].ui.spinbox_length.setValue(cylinder_length)
+    app.window.navigationmodel.views[1].action_apply_settings() # apply the above settings
+
+    # Channels View values
+    channels_diameter = channels_diameter = values_dict.get("CONFIG_CHANNELS_DIAMETER")
+    needle_length = channels_diameter = values_dict.get("CONFIG_NEEDLE_LENGTH")
+    app.window.navigationmodel.views[2].ui.spinbox_diameter.setValue(channels_diameter)
+    app.window.navigationmodel.views[2].ui.sb_needle_length.setValue(needle_length)
+    app.window.navigationmodel.views[2].action_apply_settings() # apply the above settings
+    # note for above: the needle lengths do not affect the view at all.  They are only used when
+    # generating the .pdf Reference sheet.
+
+    # Tandem View values
+    tandem_tip_height = values_dict.get("CONFIG_TANDEM_TIP_HEIGHT")
+    tandem_channel_diameter = values_dict.get("CONFIG_TANDEM_CHANNEL_DIAMETER")
+    tandem_stopper_diameter = values_dict.get("CONFIG_TANDEM_STOPPER_DIAMETER")
+    tandem_tip_angle = values_dict.get("CONFIG_TANDEM_TIP_ANGLE")
+    tandem_bend_radius = values_dict.get("CONFIG_TANDEM_BEND_RADIUS")
+
+    app.window.navigationmodel.views[3].ui.sb_tandem_height.setValue(tandem_tip_height)
+    app.window.navigationmodel.views[3].ui.sp_channel_diameter.setValue(tandem_channel_diameter)
+    app.window.navigationmodel.views[3].ui.sp_stopper_diameter.setValue(tandem_stopper_diameter)
+    app.window.navigationmodel.views[3].ui.sp_bend_angle.setValue(tandem_tip_angle)
+    app.window.navigationmodel.views[3].ui.sb_bend_radius.setValue(tandem_bend_radius)
+
+    # if there is a tandem already generated, update its values.  If not, don't generate one.
+    # if tandem exists
+    if app.window.tandemmodel._base_shape: # FIND A NEW WAY TO DO THIS BC OF _ AT BEGINNING****************************
+        # if tandem is generated rather than imported
+        if not app.window.tandemmodel.is_shape_imported:
+            # apply the config settings
+            app.window.navigationmodel.views[3].action_set_tandem()
+
+
+    
 
 
