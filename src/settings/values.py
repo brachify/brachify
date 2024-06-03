38a9db4b3bc4f6382a5aa5d876239865d8c18131
stephaniemerkl <s.merkl@telus.net>
2024-06-03 15:55:25 -0700

    Move resetAllValues() method from load.py to values.py so belongs to Values class.

--- src/settings/values.py
+++ src/settings/values.py
@@ -1,8 +1,87 @@
 # object to store all the config and default values
 from settings import load
 from settings.load import DEFAULT_CONFIG_VALUES
+from classes.info import USER_PATH
+from classes.logger import log
+from classes.app import get_app
 
 class Values():
+
+    def rememberConfigFilePaths(self):
+        """
+        Reads and writes a .json file that stores the filepaths for which config files were most recently used and saved.
+        """  
+
+        # Search in the location where the log is stored to find a .json file called filepaths.json.
+        file_name = USER_PATH.joinpath("filepaths.json")
+
+
+        try:
+            # open, read, parse as dictionary, and close the filepaths.json file
+            file = open(file_name)
+            file_paths = json.load(file)
+            file.close()       
+            log.debug("Successfully loaded the filepaths.json file.")
+
+        except:
+            # if can't read default settings from filepaths.json file, then ??????
+            log.debug("Couldn't read from filepaths.json.")
+
+
+    def resetAllValues(self, values_dict: dict):
+        """
+        Resets all values in spin boxes to the values in the given dictionary and applies them to the views.
+        """
+
+        
+        app = get_app()
+
+        # set all the spin box values and apply the changes
+        """
+        Steps:
+        1. read the value from the dictionary
+        2. set the spin box value
+        3. apply the value
+        """
+
+        # Cylinder View values
+        cylinder_diameter = values_dict.get("CONFIG_CYLINDER_DIAMETER")
+        cylinder_length = values_dict.get("CONFIG_CYLINDER_LENGTH")
+        app.window.navigationmodel.views[1].ui.spinbox_diameter.setValue(cylinder_diameter)
+        app.window.navigationmodel.views[1].ui.spinbox_length.setValue(cylinder_length)
+        app.window.navigationmodel.views[1].action_apply_settings() # apply the above settings
+
+        # Channels View values
+        channels_diameter = channels_diameter = values_dict.get("CONFIG_CHANNELS_DIAMETER")
+        needle_length = channels_diameter = values_dict.get("CONFIG_NEEDLE_LENGTH")
+        app.window.navigationmodel.views[2].ui.spinbox_diameter.setValue(channels_diameter)
+        app.window.navigationmodel.views[2].ui.sb_needle_length.setValue(needle_length)
+        app.window.navigationmodel.views[2].action_apply_settings() # apply the above settings
+        # note for above: the needle lengths do not affect the view at all.  They are only used when
+        # generating the .pdf Reference sheet.
+
+        # Tandem View values
+        tandem_tip_height = values_dict.get("CONFIG_TANDEM_TIP_HEIGHT")
+        tandem_channel_diameter = values_dict.get("CONFIG_TANDEM_CHANNEL_DIAMETER")
+        tandem_stopper_diameter = values_dict.get("CONFIG_TANDEM_STOPPER_DIAMETER")
+        tandem_tip_angle = values_dict.get("CONFIG_TANDEM_TIP_ANGLE")
+        tandem_bend_radius = values_dict.get("CONFIG_TANDEM_BEND_RADIUS")
+
+        app.window.navigationmodel.views[3].ui.sb_tandem_height.setValue(tandem_tip_height)
+        app.window.navigationmodel.views[3].ui.sp_channel_diameter.setValue(tandem_channel_diameter)
+        app.window.navigationmodel.views[3].ui.sp_stopper_diameter.setValue(tandem_stopper_diameter)
+        app.window.navigationmodel.views[3].ui.sp_bend_angle.setValue(tandem_tip_angle)
+        app.window.navigationmodel.views[3].ui.sb_bend_radius.setValue(tandem_bend_radius)
+
+        # if there is a tandem already generated, update its values.  If not, don't generate one.
+        # if tandem exists
+        if app.window.tandemmodel._base_shape: # FIND A NEW WAY TO DO THIS BC OF _ AT BEGINNING****************************
+            # if tandem is generated rather than imported
+            if not app.window.tandemmodel.is_shape_imported:
+                # apply the config settings
+                app.window.navigationmodel.views[3].action_set_tandem()
+
+
 
     def createConfigMessageText(self):
         """
