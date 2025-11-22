71b2b086c9e183f96d6f6e0657cb64d0d9945136
Timesyrup <reece_w_alexander@hotmail.com>
2025-11-22 04:27:39 -0800

    Remove changes to reset.py for the config file portion as this will be its own branch/Pr.

--- src/settings/reset.py
+++ src/settings/reset.py
@@ -91,10 +91,6 @@ def resetAllValues(values_dict: dict):
         if not app.window.tandemmodel.is_shape_imported:
             # apply the config settings
             app.window.navigationmodel.views[3].action_set_tandem()
-
-    # Notch View Values
-    notch_length = values_dict.get("CONFIG_NOTCH_LENGTH")
-    notch_height = values_dict.get("CONFIG_NOTCH_HEIGHT")
     
 def getCurrentValues():
     """ 
@@ -105,8 +101,6 @@ def getCurrentValues():
     # 1. cylinder data
     default_cylinder_diameter = app.window.cylindermodel.cylinder.diameter
     default_length = app.window.cylindermodel.cylinder.length
-    default_notch_length = app.window.cylindermodel.cylinder.notch.length
-    default_notch_height = app.window.cylindermodel.cylinder.notch.height
     # 2. channels data
     default_diameter = app.window.channelsmodel.diameter
     threading_depth = app.window.channelsmodel.threading_depth
@@ -140,9 +134,7 @@ def getCurrentValues():
         "CONFIG_CHANNELS_THREADING_DIAMETER": threading_diamenter,
         "CONFIG_TANDEM_ROTATION": tandem_rotation,
         "CONFIG_TANDEM_THREADING_DEPTH": tandem_threading_depth,
-        "CONFIG_TANDEM_THREADING_DIAMETER": tandem_threading_diameter,
-        "CONFIG_NOTCH_LENGTH": default_notch_length,
-        "CONFIG_NOTCH_HEIGHT": default_notch_height
+        "CONFIG_TANDEM_THREADING_DIAMETER": tandem_threading_diameter
     }
 
     return current_values
