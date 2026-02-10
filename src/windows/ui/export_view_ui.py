82ead603cb17eb4e515ed3f6a68b2e4940751abd
Timesyrup <reece_w_alexander@hotmail.com>
2026-02-10 10:12:45 -0800

    Ui fixes.

--- src/windows/ui/export_view_ui.py
+++ src/windows/ui/export_view_ui.py
@@ -23,7 +23,7 @@ class Ui_Export_View(object):
     def setupUi(self, Export_View):
         if not Export_View.objectName():
             Export_View.setObjectName(u"Export_View")
-        Export_View.resize(290, 290)
+        Export_View.resize(290, 312)
         sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Minimum)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -52,56 +52,54 @@ class Ui_Export_View(object):
 
         self.label = QLabel(Export_View)
         self.label.setObjectName(u"label")
+        self.label.setStyleSheet(u"background-color: rgb(240, 245, 250);")
 
         self.formLayout.setWidget(1, QFormLayout.LabelRole, self.label)
+
+        self.label_3 = QLabel(Export_View)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setStyleSheet(u"background-color: rgb(240, 245, 250);")
+
+        self.formLayout.setWidget(3, QFormLayout.LabelRole, self.label_3)
+
+        self.label_4 = QLabel(Export_View)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setStyleSheet(u"background-color: rgb(240, 245, 250);")
+
+        self.formLayout.setWidget(4, QFormLayout.LabelRole, self.label_4)
+
+        self.label_5 = QLabel(Export_View)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setStyleSheet(u"background-color: rgb(240, 245, 250);")
+
+        self.formLayout.setWidget(5, QFormLayout.LabelRole, self.label_5)
+
+        self.sb_tandem_collet_inner_od = QDoubleSpinBox(Export_View)
+        self.sb_tandem_collet_inner_od.setObjectName(u"sb_tandem_collet_inner_od")
+
+        self.formLayout.setWidget(5, QFormLayout.FieldRole, self.sb_tandem_collet_inner_od)
+
+        self.sb_tandem_collet_outer_od = QDoubleSpinBox(Export_View)
+        self.sb_tandem_collet_outer_od.setObjectName(u"sb_tandem_collet_outer_od")
+
+        self.formLayout.setWidget(4, QFormLayout.FieldRole, self.sb_tandem_collet_outer_od)
+
+        self.sb_needle_collet_od = QDoubleSpinBox(Export_View)
+        self.sb_needle_collet_od.setObjectName(u"sb_needle_collet_od")
+
+        self.formLayout.setWidget(3, QFormLayout.FieldRole, self.sb_needle_collet_od)
 
         self.cb_collet_preview_reference_sheet = QCheckBox(Export_View)
         self.cb_collet_preview_reference_sheet.setObjectName(u"cb_collet_preview_reference_sheet")
         self.cb_collet_preview_reference_sheet.setLayoutDirection(Qt.RightToLeft)
         self.cb_collet_preview_reference_sheet.setAutoFillBackground(False)
-        self.cb_collet_preview_reference_sheet.setStyleSheet(u"background-color: rgb(230, 238, 246);")
+        self.cb_collet_preview_reference_sheet.setStyleSheet(u"background-color: rgb(240, 245, 250);")
         self.cb_collet_preview_reference_sheet.setChecked(True)
 
         self.formLayout.setWidget(1, QFormLayout.FieldRole, self.cb_collet_preview_reference_sheet)
 
 
         self.verticalLayout.addLayout(self.formLayout)
-
-        self.formLayout_2 = QFormLayout()
-        self.formLayout_2.setObjectName(u"formLayout_2")
-        self.formLayout_2.setContentsMargins(43, -1, 0, 0)
-        self.label_3 = QLabel(Export_View)
-        self.label_3.setObjectName(u"label_3")
-
-        self.formLayout_2.setWidget(0, QFormLayout.LabelRole, self.label_3)
-
-        self.sb_needle_collet_od = QDoubleSpinBox(Export_View)
-        self.sb_needle_collet_od.setObjectName(u"sb_needle_collet_od")
-
-        self.formLayout_2.setWidget(0, QFormLayout.FieldRole, self.sb_needle_collet_od)
-
-        self.label_4 = QLabel(Export_View)
-        self.label_4.setObjectName(u"label_4")
-
-        self.formLayout_2.setWidget(1, QFormLayout.LabelRole, self.label_4)
-
-        self.label_5 = QLabel(Export_View)
-        self.label_5.setObjectName(u"label_5")
-
-        self.formLayout_2.setWidget(2, QFormLayout.LabelRole, self.label_5)
-
-        self.sb_tandem_collet_inner_od = QDoubleSpinBox(Export_View)
-        self.sb_tandem_collet_inner_od.setObjectName(u"sb_tandem_collet_inner_od")
-
-        self.formLayout_2.setWidget(2, QFormLayout.FieldRole, self.sb_tandem_collet_inner_od)
-
-        self.sb_tandem_collet_outer_od = QDoubleSpinBox(Export_View)
-        self.sb_tandem_collet_outer_od.setObjectName(u"sb_tandem_collet_outer_od")
-
-        self.formLayout_2.setWidget(1, QFormLayout.FieldRole, self.sb_tandem_collet_outer_od)
-
-
-        self.verticalLayout.addLayout(self.formLayout_2)
 
         self.verticalSpacer_2 = QSpacerItem(20, 10, QSizePolicy.Minimum, QSizePolicy.Fixed)
 
@@ -202,11 +200,11 @@ class Ui_Export_View(object):
         Export_View.setWindowFilePath("")
         self.label_2.setText(QCoreApplication.translate("Export_View", u"Show Tandem", None))
         self.cb_tandem_shown.setText("")
-        self.label.setText(QCoreApplication.translate("Export_View", u"Include Collet In Reference Sheet ", None))
+        self.label.setText(QCoreApplication.translate("Export_View", u"Show Collet Spacings", None))
+        self.label_3.setText(QCoreApplication.translate("Export_View", u"Needle Collet Spacing Tol.", None))
+        self.label_4.setText(QCoreApplication.translate("Export_View", u"Tandem Outer Spacing Tol.", None))
+        self.label_5.setText(QCoreApplication.translate("Export_View", u"Tandem Inner Spacing Tol.", None))
         self.cb_collet_preview_reference_sheet.setText("")
-        self.label_3.setText(QCoreApplication.translate("Export_View", u"Needle Collet", None))
-        self.label_4.setText(QCoreApplication.translate("Export_View", u"Tandem Outer Collet", None))
-        self.label_5.setText(QCoreApplication.translate("Export_View", u"Tandem Inner Collet", None))
         self.btn_export_mesh.setText(QCoreApplication.translate("Export_View", u"Export Mesh", None))
         self.btn_export_template_reference.setText(QCoreApplication.translate("Export_View", u"Export Reference Sheet", None))
         self.btn_export_shapes.setText(QCoreApplication.translate("Export_View", u"Export Shape(s)", None))
